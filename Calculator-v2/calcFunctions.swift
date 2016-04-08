f3b78ca7186faafbec496e0e06738081a7d77f5e
Tanner Garrett <tanner@garrett.org>
2016-04-07 17:10:52 -0700

    Completed Assignment

---
+++ Calculator-v2/calcFunctions.swift
@@ -0,0 +1,131 @@
+//
+//  calcFunctions.swift
+//  Calculator-v2
+//
+//  Created by Tanner on 4/7/16.
+//  Copyright © 2016 Tanner. All rights reserved.
+//
+//  Functions needed in the Calculator and More assignment
+
+import Foundation
+
+// A generic "math operation" function that takes in two Doubles and passes them to a given function,
+// returning the resulting Double
+func mathOp(leftOperator: Double, rightOperator: Double, op: (Double, Double) -> Double) -> Double {
+    return op(leftOperator, rightOperator);
+}
+
+// An addition function that takes in two Doubles and returns the sum of both as a Double
+func additionOp(leftOperator: Double, rightOperator: Double) -> Double {
+    return (leftOperator + rightOperator);
+}
+
+// A subtraction function that takes in two Doubles and returns the difference of both as a Double
+func subtractionOp(leftOperator: Double, rightOperator: Double) -> Double {
+    return (leftOperator - rightOperator);
+}
+
+// A multiplication function that takes in two Doubles and returns the product of both as a Double
+func multiplicationOp(leftOperator: Double, rightOperator: Double) -> Double {
+    return (leftOperator * rightOperator);
+}
+
+// A division function that takes in two Doubles and returns the quotient of both as a Double
+func divisionOp(leftOperator: Double, rightOperator: Double) -> Double {
+    return (leftOperator / rightOperator);
+}
+
+// A mod function that takes in two Doubles and returns the mod of the first mod the second as a Double
+func modOp(leftOperator: Double, rightOperator: Double) -> Double {
+    return (leftOperator % rightOperator);
+}
+
+// A generic "array operation" function that takes in an array of doubles and passes them to a given function,
+// returning the resulting Double
+func arrayOp(doubleArray: [Double], op: ([Double]) -> Double) -> Double {
+    return op(doubleArray);
+}
+
+// An addition function that takes in an array of Doubles and returns the sum of each Double in the array as
+// A single Double
+func addArrayOp(doubleArray: [Double]) -> Double {
+    return doubleArray.reduce(0, combine: +);
+}
+
+// A multiplication function that takes in an array of Doubles and returns the product of each Double in the array as
+// A single Double
+func multArrayOp(doubleArray: [Double]) -> Double {
+    return doubleArray.reduce(1.0, combine: *);
+}
+
+// A counting function that takes in an array of Doubles and returns the count of how many elements were in the 
+// array passed in as a single Double
+func countArrayOp(doubleArray: [Double]) -> Double {
+    return Double(doubleArray.count);
+}
+
+// An averaging function that takes in an array of Doubles and returns the average of the Doubles in the array
+// passed in
+func avgArrayOp(doubleArray: [Double]) -> Double {
+    let sum = addArrayOp(doubleArray);
+    return sum / Double(doubleArray.count);
+}
+
+// Adds two points together given two tuples made of the form (Int, Int) and returns the resultingtuple of the same form
+func pointAddition(pointOne: (x: Int, y: Int), pointTwo: (x: Int, y: Int)) -> (x: Int, y: Int) {
+    return (nilToZero(pointOne.x) + nilToZero(pointTwo.x), nilToZero(pointOne.y) + nilToZero(pointTwo.y));
+}
+
+// Adds two points together given two tuples made of the form (Int, Int) and returns the resultingtuple of the same form
+func pointSubtraction(pointOne: (x: Int, y: Int), pointTwo: (x: Int, y: Int)) -> (x: Int, y: Int) {
+    return (pointOne.x - pointTwo.x, pointOne.y + pointTwo.y);
+}
+
+// Adds two given Dictionaries with the keys being String and the values being Doubles
+func dictAddition(dictOne: Dictionary<String, Double>, dictTwo: Dictionary<String, Double>) -> Dictionary<String, Double> {
+    let safeDictOne = dictNilToZero(dictOne);
+    let safeDictTwo = dictNilToZero(dictTwo);
+    let finalDict = [
+        "x" : safeDictOne["x"]! + safeDictTwo["x"]!,
+        "y" : safeDictOne["y"]! + safeDictTwo["y"]!,
+    ]
+    return finalDict;
+}
+
+// Subtracts two given Dictionaries with the keys being String and the values being Doubles
+func dictSubtraction(dictOne: Dictionary<String, Double>, dictTwo: Dictionary<String, Double>) -> Dictionary<String, Double> {
+    let safeDictOne = dictNilToZero(dictOne);
+    let safeDictTwo = dictNilToZero(dictTwo);
+    let finalDict = [
+        "x" : safeDictOne["x"]! - safeDictTwo["x"]!,
+        "y" : safeDictOne["y"]! - safeDictTwo["y"]!,
+    ]
+    return finalDict;
+}
+
+// Takes in an optional Int and returns an Int, changing the value nil to 0
+func nilToZero(optional: Int?) -> Int {
+    if optional != nil {
+        return optional!;
+    } else {
+        return 0;
+    }
+}
+
+// Takes a given dictionary with the keys being Strings and the values being Doubles. It returns a new Dictionary with the
+// given values for "x" and "y", or zero if they are not set
+func dictNilToZero(dict: Dictionary<String, Double>) -> Dictionary<String, Double> {
+    var noNilDict = [
+        "x": 0.0,
+        "y": 0.0,
+    ];
+    let dictX = dict["x"];
+    let dictY = dict["y"];
+    if dict["x"] != nil {
+        noNilDict["x"] = dictX;
+    }
+    if dict["y"] != nil {
+        noNilDict["y"] = dictY;
+    }
+    return noNilDict;
+}
---
+++ Calculator-v2/main.swift
@@ -0,0 +1,152 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Tanner on 4/7/16.
+//  Copyright © 2016 Tanner. All rights reserved.
+//
+//  This program allows a user to input commands in order to do simple operations such as addition, multiplication
+//  subtraction and modulation. It also supports averaging, counting, and computing factorials. All operations except
+//  factorial support the input of doubles and negative numbers. The program will continue to cycle until the user
+//  specifies they do not want to continue playing.
+//  CONTAINS CHANGES FOR THE CALCULATORS AND MORE ASSIGNMENT
+
+import Foundation
+
+// Contains logic that prints out computations and instructions depending on the input the
+// user provides. Returns True if the user wants to continue inputting computations and
+// False if they want to terminate the program
+
+// Testing for the "Calculator and More" Assignment, to prove the functions work
+var tupleOne = (x: 2, y: 0);
+var tupleTwo = (x: 2, y: -4);
+var tupleSolution = pointAddition(tupleOne, pointTwo: tupleTwo);
+print(tupleSolution);
+
+var dictOne = [
+    "x": 5.0,
+    "y": 5.0,
+];
+
+var dictTwo = [
+    "x": 2.0,
+];
+
+var dictAdd = dictAddition(dictOne, dictTwo: dictTwo);
+print(dictAdd);
+
+var dictSub = dictSubtraction(dictOne, dictTwo: dictTwo);
+print(dictSub);
+
+// Contains logic to ask the user to make a computation and prints them their correct answers
+// IMPLEMENTS CHANGES FROM CALCULATOR AND MORE ASSIGNMENT
+func computation() -> Bool {
+    print("Please Insert Your Computation :");
+    let firstPassUI = input();
+    let firstPassArray = firstPassUI.characters.split{$0 == " "}.map(String.init);
+    let firstPassEndIndex = removeNewline((firstPassArray.last)!);
+    var doubleArray: Array<Double> = [];
+    if firstPassArray.count > 1 {
+        for index in 0...firstPassArray.count - 2 {
+            let doubleString = Double(firstPassArray[index]);
+            doubleArray.append(doubleString!);
+        }
+    }
+    if(firstPassArray.count == 1) {
+        let trimmedDouble = removeNewline(firstPassArray[0]);
+        let doubleString:Double? = Double(trimmedDouble);
+        operationComputation(doubleString!);
+        // Test and Logic for count
+    } else if (firstPassEndIndex.lowercaseString == "count") {
+        print("The count is : \(arrayOp(doubleArray, op: countArrayOp))");
+    } else if (firstPassEndIndex.lowercaseString == "avg") {
+        print("The average is :  \(arrayOp(doubleArray, op: avgArrayOp))");
+    } else if (firstPassEndIndex.lowercaseString == "add") {
+        print("The sum is is :  \(arrayOp(doubleArray, op: addArrayOp))");
+    } else if (firstPassEndIndex.lowercaseString == "mult") {
+        print("The product is :  \(arrayOp(doubleArray, op: multArrayOp))");
+    } else if (firstPassEndIndex.lowercaseString == "fact") {
+        if(firstPassArray.count != 2) {
+            print("Please format a factorial call with the given format : '[Integer] fact'");
+        } else {
+            print("\(factorial(Int(firstPassArray[0])!))");
+        }
+        
+    } else {
+        print("You put in an invalid query");
+    }
+    return continueQuery();
+}
+
+// Contains the logic for simple addition, subtraction, division, multiplication,
+// and modulation - printing out information to the user as is appropriate
+// IMPLEMENTS CHANGES FROM CALCULATOR AND MORE ASSIGNMENT
+func operationComputation(numberOne: Double) {
+    print("Please add an Operator :");
+    let operatorString = removeNewline(input());
+    print("Insert an Additional Operand :");
+    let numberTwo: Double? = Double(removeNewline(input()));
+    if (operatorString == "+") {
+        print("\(numberOne) + \(numberTwo!) = \(mathOp(numberOne, rightOperator: numberTwo!, op: additionOp))");
+    } else if (operatorString == "-") {
+        print("\(numberOne) - \(numberTwo!) = \(mathOp(numberOne, rightOperator: numberTwo!, op: subtractionOp))");
+    } else if (operatorString == "*") {
+        print("\(numberOne) * \(numberTwo!) = \(mathOp(numberOne, rightOperator: numberTwo!, op: multiplicationOp))");
+    } else if (operatorString == "/") {
+        print("\(numberOne) / \(numberTwo!) = \(mathOp(numberOne, rightOperator: numberTwo!, op: divisionOp))");
+    } else if (operatorString == "%") {
+        print("\(numberOne) % \(numberTwo!) = \(mathOp(numberOne, rightOperator: numberTwo!, op: modOp))");
+    } else {
+        print("Please use one of the accepted operators: + - / %")
+    }
+}
+
+// Removes the last two characters of whatever String is passed in, returning the resulting
+// String
+func removeNewline(newlineString: String) -> String {
+    var trimmedString = newlineString;
+    let range = trimmedString.endIndex.advancedBy(-1)..<trimmedString.endIndex;
+    trimmedString.removeRange(range);
+    return trimmedString;
+}
+
+// Given an Int, returns an Int that is the result of a factorial operation on the Int
+// passed in
+func factorial(factInt: Int) -> Int {
+    if(factInt <= 1) {
+        return 1;
+    } else {
+        return factInt * factorial(factInt - 1);
+    }
+}
+
+// Queries the user on whether or not they want to complete another operation. Returns True
+// if they respond with yes and False if they respond with no
+func continueQuery() -> Bool {
+    print("Would you like to make another query?");
+    var continueAnswer = "";
+    // Continue asking the user if they want to make another query until they answer "yes" or "no"
+    while (continueAnswer.lowercaseString != "yes" && continueAnswer.lowercaseString != "no") {
+        print("Please answer 'Yes' or 'No'");
+        continueAnswer = removeNewline(input());
+    }
+    return (continueAnswer.lowercaseString == "yes");
+    
+}
+
+// Returns an NSString depending on what the user inputs using the keyboard
+func input() -> String{
+    let keyboard = NSFileHandle.fileHandleWithStandardInput();
+    let inputData = keyboard.availableData;
+    return NSString(data: inputData, encoding: NSUTF8StringEncoding) as! String;
+}
+
+// Starts the cycle of computations, and terminates once the user decides they want to stop
+
+var cycle = true;
+
+while cycle {
+    cycle = computation();
+}
+
+
